071fc47a468c91223f53185e7304f23f3f796f93
humayun-1 <humayunspam143@gmail.com>
2022-11-13 02:00:37 +0500

    icon size

--- src/Components/Common/CircleIcon.jsx
+++ src/Components/Common/CircleIcon.jsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 const CircleIcon = (props) => {
     return (
         <div onClick={props.onClick} className='flex items-center flex-col gap-2 cursor-pointer'>
-            <div className={`bg-white transition-all hover:bg-[#ffd4ef] ${props.size ? props.size : 'h-[6rem] w-[6rem]'} rounded-full flex items-center justify-center`}>
+            <div className={`bg-white transition-all hover:bg-[#ffd4ef] ${props.size ? props.size : 'h-[8rem] w-[8rem]'} rounded-full flex items-center justify-center`}>
                 {props.icon}
             </div>
             {props.text && <p className=''>{props.text}</p>}
