cd6c87b1f1be41582f74ca1889a9c5e91c1d6f65
humayun-1 <humayunspam143@gmail.com>
2022-11-13 13:27:14 +0500

    intro final

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import AppWrapper from './Common/AppWrapper';
 import Intro from './Steps/Intro';
+import Lang from './Steps/Lang';
 import One from './Steps/One';
 import Two from './Steps/Two';
 import Three from './Steps/Three';
@@ -21,35 +22,43 @@ const Main = () => {
             }
 
             {
+                Active == 11 && <Lang Active={Active} setActive={setActive} />
+            }
+
+            {
                 Active == 1 && <One Active={Active} setActive={setActive} />
             }
 
             {
-                Active == 2 && <Two Active={Active} setActive={setActive} />
+                Active == 2 && <One Active={Active} setActive={setActive} />
             }
 
             {
-                Active == 3 && <Three Active={Active} setActive={setActive} />
+                Active == 3 && <Two Active={Active} setActive={setActive} />
             }
 
             {
-                Active == 4 && <Four Active={Active} setActive={setActive} />
+                Active == 4 && <Three Active={Active} setActive={setActive} />
             }
 
             {
-                Active == 5 && <Five Active={Active} setActive={setActive} />
+                Active == 5 && <Four Active={Active} setActive={setActive} />
             }
 
             {
-                Active == 6 && <Six Active={Active} setActive={setActive} />
+                Active == 6 && <Five Active={Active} setActive={setActive} />
             }
 
             {
-                Active == 8 && <Eight Active={Active} setActive={setActive} />
+                Active == 7 && <Six Active={Active} setActive={setActive} />
             }
 
             {
-                Active == 7 && <Seven Active={Active} setActive={setActive} />
+                Active == 9 && <Eight Active={Active} setActive={setActive} />
+            }
+
+            {
+                Active == 10 && <Seven Active={Active} setActive={setActive} />
             }
         </AppWrapper>
     )
--- src/Components/Steps/Intro.jsx
+++ src/Components/Steps/Intro.jsx
@@ -4,18 +4,29 @@ import CircleIcon from '../Common/CircleIcon'
 import Svgs from '../Common/Svgs'
 
 const Intro = (props) => {
+    const [checked, setchecked] = useState(false)
+
     const handleChange = (param) => {
-        props.setActive(props.Active + 1)
+        setchecked(!checked)
+        setTimeout(() => {
+            props.setActive(props.Active + 1)
+        }, 400);
     }
+
     return (
-        <div className='slide-in-elliptic-top-fwd flex flex-col gap-6 items-center bg-pink-500/30 backdrop-blur-[6px] w-fit mx-auto p-[3rem] rounded-2xl'>
-            <h1 className='text-3xl font-extrabold'>
-                Hi, how was your experience at Savour Foods today?
+        <div className={`${checked && "scale-out-center"} slide-in-elliptic-top-fwd flex flex-col gap-6 items-center bg-pink-500/30 backdrop-blur-[6px] w-fit mx-auto p-[3rem] rounded-2xl`}>
+            <h1 className='text-[2.4rem] font-extrabold'>
+                Customer Care
             </h1>
-            <h1 className='text-3xl font-extrabold font-sans'>
-                -سلام, آج آپ کا سیور کا تجربہ کیسا رہا
-            </h1>
-            <button className='hover:bg-[#D55AA3] border border-white px-6 py-2 rounded-lg transition-all bg-white text-[#D55AA3] ease-out font-semibold text-lg hover:text-white'>Quick Start</button>
+            <div className='flex flex-col gap-2'>
+                <h1 className='text-xl font-extrabold'>
+                    Connect with the Top Management
+                </h1>
+                <h1 className='text-xl font-extrabold font-sans'>
+                    اعلیٰ انتظامیہ سے جڑیں۔
+                </h1>
+            </div>
+            <button onClick={handleChange} className='hover:bg-[#D55AA3] border border-white px-6 py-2 rounded-lg transition-all bg-white text-[#D55AA3] ease-out font-semibold text-lg hover:text-white'>Quick Start</button>
         </div>
     )
 }
